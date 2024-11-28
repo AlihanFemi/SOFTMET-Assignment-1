510da34ea54826a4074f8748995a734b1b66ec23
AlihanFemi <alihanfemiofficial@gmail.com>
2024-11-28 16:42:33 +0200

    Used pylint and flake8 to make my code prettier

--- assignment2.py
+++ assignment2.py
@@ -1,11 +1,13 @@
-import unittest, json
+import unittest
+import json
+from collections import defaultdict
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from collections import defaultdict
 
 ORIGINAL_URL = 'https://en.wikipedia.org/wiki/Software_metric'
 CYCLES = 10
+
 
 class TestGetPerformance(unittest.TestCase):
     def setUp(self):
@@ -13,9 +15,9 @@ class TestGetPerformance(unittest.TestCase):
         self.cycles = CYCLES
 
     def test_performance(self):
-        # Getting and preparing the data! 
+        # Getting and preparing the data!
         map_of_urls = defaultdict(list)
-        for x in range(self.cycles):
+        for _ in range(self.cycles):
             opts = webdriver.ChromeOptions()
             opts.add_argument("--incognito")
             driver = webdriver.Chrome(options=opts)
@@ -27,23 +29,21 @@ class TestGetPerformance(unittest.TestCase):
             for key, value in data:
                 map_of_urls[key].append(value)
             driver.quit()
-        
-        #testing if there are 10 values
-        first_key = next(iter(map_of_urls))
-        first_value = map_of_urls[first_key]
-        self.assertEqual(len(first_value), CYCLES)
-        
+
+        # Testing if there are 10 values
+        self.assertEqual(len(map_of_urls[next(iter(map_of_urls))]), CYCLES)
+
         # Saving the data!
         json_string = json.dumps(map_of_urls, indent=4)
-        with open(f"./dataMap.json", 'w') as file:
+        with open("./dataMap.json", 'w', encoding='utf8') as file:
             file.writelines(json_string)
-        
+
         # Testing my formula for calculating averages
-        test_avg = [1, 2, 3, 4, 5] # Avarage should be 3
+        test_avg = [1, 2, 3, 4, 5]  # Avarage should be 3
         arr = list(filter(lambda x: x != 0, test_avg))
-        res =  sum(arr) / len(arr)
+        res = sum(arr) / len(arr)
         self.assertEqual(res, 3)
-        
+
         # Processing the data!
         map_of_averages = defaultdict(list)
         for key, value in map_of_urls.items():
@@ -51,22 +51,19 @@ class TestGetPerformance(unittest.TestCase):
             if len(arr) == 0:
                 map_of_averages[key].append(0)
                 continue
-            average_value = sum(arr) / len(arr)
-            map_of_averages[key].append(average_value)
-        
-        #testing if there is only one value
-        first_key = next(iter(map_of_averages))
-        first_value = map_of_averages[first_key]
-        self.assertEqual(len(first_value), 1)
-        
+            map_of_averages[key].append(sum(arr) / len(arr))
+
+        # Testing if there is only one value
+        self.assertEqual(len(map_of_averages[next(iter(map_of_averages))]), 1)
+
         # Saving the data!
         json_string = json.dumps(map_of_averages, indent=4)
-        with open(f"./processedMap.json", 'w') as file:
+        with open("./processedMap.json", 'w', encoding='utf8') as file:
             file.writelines(json_string)
-        
+
     def tearDown(self) -> None:
         print("done")
-    
-if __name__ == "__main__":    
+
+
+if __name__ == "__main__":
     unittest.main()
-    
